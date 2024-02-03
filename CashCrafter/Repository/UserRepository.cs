520f2fa8c55c7a5ead55c6948ad0bd358a9039b2
JK <colegioret@hotmail.com>
2024-02-03 12:17:23 -0500

    Se cre el CRUD completo DE User

---
+++ CashCrafter/Repository/UserRepository.cs
@@ -0,0 +1,57 @@
+﻿using AutoMapper;
+using CashCrafter.Api.Context;
+using CashCrafter.Api.DTO;
+using CashCrafter.Api.Interfaces;
+using CashCrafter.Api.Model;
+using Microsoft.EntityFrameworkCore;
+namespace CashCrafter.Api.Repository
+{
+    public class UserRepository : IUserRepository
+    {
+        private readonly AppDbContext _context;
+        private readonly IMapper _mapper;
+        public UserRepository(AppDbContext context, IMapper mapper)
+        {
+            _context = context;
+            _mapper = mapper;
+        }
+
+        public async Task<List<UserDTO>> Get()
+        {
+            var data = await _context.Users.ToListAsync();
+            var dto = _mapper.Map<List<UserDTO>>(data);
+            return dto;
+        }
+        public async Task<UserDTO> GetById(int Id)
+        {
+            var data = await _context.Users.FirstOrDefaultAsync(x => x.Id == Id);
+            var dto = _mapper.Map<UserDTO>(data);
+            return dto;
+        }
+        public async Task<UserDTO> GetByName(string nick)
+        {
+            var data = await _context.Users.FirstOrDefaultAsync(x => x.NickName == nick);
+            var dto = _mapper.Map<UserDTO>(data);
+            return dto;
+        }
+        public async Task<int> Create(PostUserDTO dto)
+        {
+            var data = _mapper.Map<User>(dto);
+            _context.Add(data);
+            return await _context.SaveChangesAsync();
+        }
+        public async Task<int> Update(int id, PutUserDTO dto)
+        {
+            var data = await _context.Users.FirstOrDefaultAsync(x => x.Id == id);
+            _mapper.Map(dto, data);
+            _context.Update(data);
+            return await _context.SaveChangesAsync();
+        }
+        public async Task<int> Delete(int id)
+        {
+            var data = await _context.Users.FirstOrDefaultAsync(x => x.Id == id);
+            _context.Remove(data);
+            return await _context.SaveChangesAsync();
+        }
+    }
+}
---
+++ CashCrafter/Service/Base/Mensaje.cs
@@ -0,0 +1,10 @@
+﻿using CashCrafter.Api.Interfaces;
+
+namespace CashCrafter.Api.Service
+{
+    public class Mensaje : IMensaje
+    {
+        public string Informacion { get; set; }
+        public int StatusCode { get; set; }
+    }
+}
