fa7aee9021d45178b63608200b15c0bc4c41a522
jk <colegioret@hotmail.com>
2024-02-10 17:53:09 -0500

    Se termina ingresosController

--- CashCrafter/Controller/IngresosController.cs
+++ CashCrafter/Controller/IngresosController.cs
@@ -15,5 +15,65 @@ namespace CashCrafter.Api.Controller
         {
             _ingresoService = ingresoService;
         }
+
+
+        [HttpGet]
+        public async Task<ActionResult> Get()
+        {
+            var dto = await _ingresoService.Get();
+
+            if (_ingresoService.StatusCode != 200)
+                return StatusCode(_ingresoService.StatusCode, _ingresoService.Informacion);
+
+            return Ok(new { Respuesta = dto, Mensaje = _ingresoService.Informacion });
+        }
+
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult> GetById(int id)
+        {
+            var dto = await _ingresoService.GetById(id);
+
+            if (_ingresoService.StatusCode != 200)
+                return StatusCode(_ingresoService.StatusCode, _ingresoService.Informacion);
+
+            return Ok(dto);
+        }
+
+        // POST api/<MarcasController>
+        [HttpPost]
+        public async Task<ActionResult> Create(PostIngresosDTO dto)
+        {
+            await _ingresoService.Create(dto);
+
+            if (_ingresoService.StatusCode != 200)
+                return StatusCode(_ingresoService.StatusCode, _ingresoService.Informacion);
+
+            return Ok(new { Mensaje = _ingresoService.Informacion });
+
+        }
+
+        [HttpPut("{id}")]
+        public async Task<ActionResult> Update(int id, PostIngresosDTO dto)
+        {
+            await _ingresoService.Update(id, dto);
+
+            if (_ingresoService.StatusCode != 200)
+                return StatusCode(_ingresoService.StatusCode, _ingresoService.Informacion);
+
+            return Ok(new { Mensaje = _ingresoService.Informacion });
+
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<ActionResult> Delete(int id)
+        {
+            await _ingresoService.Delete(id);
+
+            if (_ingresoService.StatusCode != 200)
+                return StatusCode(_ingresoService.StatusCode, _ingresoService.Informacion);
+
+            return Ok(new { Mensaje = _ingresoService.Informacion });
+        }
     }
 }
